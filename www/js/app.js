20645406b775508f3566c0a5335b807fb7bb0ae8
Alyson Hurt <ahurt@npr.org>
2013-05-14 10:07:16 -0400

    adjusted viz to account for offsets, but still not quote what i want

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,8 @@ var EPISODE_COUNT = 53;
 var JOKE_COUNT = 10;
 
 var DOT_RADIUS = 5;
+var OFFSET_X = DOT_RADIUS;
+var OFFSET_Y = DOT_RADIUS;
 
 var $viz = null;
 var viz_div = null;
@@ -19,14 +21,14 @@ $(function() {
 
         var paper = new Raphael(viz_div, width, height);
 
-        var line_interval = height / JOKE_COUNT;
-        var dot_interval = width / EPISODE_COUNT;
+        var line_interval = (height - (OFFSET_Y * 2)) / JOKE_COUNT;
+        var dot_interval = (width - (OFFSET_X * 2)) / EPISODE_COUNT;
 
         // Render joke lines
         for (var i = 0; i < JOKE_COUNT; i++) {
-            var line_y = i * line_interval;
+            var line_y = (i * line_interval) + OFFSET_Y;
 
-            var path = 'M' + 0 + "," + line_y + 'L' + width + ',' + line_y;
+            var path = 'M' + (0 + OFFSET_X) + "," + line_y + 'L' + (width - (OFFSET_X * 2)) + ',' + line_y;
             var line = paper.path(path)
 
             line.node.setAttribute('id', 'joke-' + i);
@@ -40,11 +42,11 @@ $(function() {
         var to_joke = 9;
         var to_episode = 2;
 
-        var from_x = from_episode * dot_interval;
-        var from_y = from_joke * line_interval;
+        var from_x = from_episode * dot_interval + OFFSET_X;
+        var from_y = from_joke * line_interval + OFFSET_Y;
 
-        var to_x = to_episode * dot_interval;
-        var to_y = to_joke * line_interval;
+        var to_x = to_episode * dot_interval + OFFSET_X;
+        var to_y = to_joke * line_interval + OFFSET_Y;
 
         var control_x1 = from_x - (dot_interval * 0.75);
         var control_y1 = from_y + line_interval;
@@ -53,15 +55,18 @@ $(function() {
         var control_y2 = to_y - line_interval;
 
         var path = 'M' + from_x + ',' + from_y + ' C'  + control_x1 + ',' + control_y1 + ' ' + control_x2 + ',' + control_y2 + ' ' + to_x + ',' + to_y;
+        var line = paper.path(path);
 
-        paper.path(path);
+        line.node.setAttribute('id', 'connection-j' + from_joke + 'e' + from_episode + '-j' + to_joke + 'e' + to_episode);
+        line.node.setAttribute('class', 'connection-line');
+
 
         // Render episode dots
         for (var i = 0; i < JOKE_COUNT; i++) {
-            var line_y = i * line_interval;
+            var line_y = i * line_interval + OFFSET_Y;
 
             for (var j = 0; j < EPISODE_COUNT; j++) {
-                var dot = paper.circle(j * dot_interval, line_y, 5); 
+                var dot = paper.circle((j * dot_interval) + OFFSET_X, line_y, 5); 
 
                 dot.node.setAttribute('id', 'joke-' + i + '-episode-' + j);
                 dot.node.setAttribute('class', 'dot episode-dot');
